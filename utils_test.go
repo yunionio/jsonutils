228df18e70c659d8f61819e0e2a8671079680774
Qiu Jian <qiujian@yunionyun.com>
2018-12-08 09:41:29 +0800

    增加 GetArrayOfPrefix 接口从json获取 prefix.0, prefix.1, ... 构成的数组

--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,9 @@
 package jsonutils
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGetQueryStringArray(t *testing.T) {
 	for _, queryString := range []string{"search=zone", "search=abc&search=123&search=b", "search.0=123&search.1=abc"} {
@@ -12,5 +15,17 @@ func TestGetQueryStringArray(t *testing.T) {
 		}
 		search := GetQueryStringArray(jsonQuery, "search")
 		t.Logf("%s", search)
+	}
+}
+
+func TestGetArrayOfPrefix(t *testing.T) {
+	json := NewDict()
+	for i := 0; i < 10; i += 1 {
+		json.Add(NewString(fmt.Sprintf("value.%d", i)), fmt.Sprintf("key.%d", i))
+	}
+
+	retArray := GetArrayOfPrefix(json, "key")
+	if len(retArray) != 10 {
+		t.Errorf("fail to getarrayofprefix")
 	}
 }
